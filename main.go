ff806ffc35cb9978dd874037a2e5036943154349
jh <neosouler@gmail.com>
2022-03-28 14:16:01 +0900

    #7.

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/neosouler7/bitGoin/rest"
-)
+import "github.com/neosouler7/bitGoin/cli"
 
 func main() {
-	fmt.Println("Welcome to bitGoin :)")
-
-	// chain := blockchain.GetBlockchain()
-	// chain.AddBlock("Second Block")
-
-	// go explorer.Start(3000)
-
-	rest.Start(4000)
+	cli.Start()
 }
